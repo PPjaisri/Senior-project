0a891d12d1ae21c59cfa209ad25f26e3e6a42b9a
PPjaisri <ppjaisri@gmail.com>
2021-11-07 23:29:04 +0700

    update cofact_refer

--- Scrapy/Scrapy_project/settings.py
+++ Scrapy/Scrapy_project/settings.py
@@ -14,7 +14,8 @@ NEWSPIDER_MODULE = 'Scrapy_project.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Scrapy_project (+https://lab107.kasetsart-university.org/)'
+# USER_AGENT = 'Scrapy_project (+https://lab107.kasetsart-university.org/)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
