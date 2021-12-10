2372f02f4386b3730d7347b04dc2ddb8bdbd7807
PPjaisri <ppjaisri@gmail.com>
2021-12-10 23:31:51 +0700

    exe file for linux

---
+++ file_runner windows.py
@@ -0,0 +1,19 @@
+import os
+
+# path = r'C:\\Users\\PPjaisri\\Coding\\Senior project'
+path = os.getcwd()
+path = os.path.dirname(path)
+path = os.path.join(path, 'Scrapy\Scrapy_project')
+
+os.chdir(path)
+
+scrape_list = [
+    'anti_thread',
+    'anti_info',
+    'cofact_thread',
+    'cofact_info',
+    'cofact_refer'
+    ]
+
+for i in scrape_list:
+    os.system(f'scrapy crawl {i}')
