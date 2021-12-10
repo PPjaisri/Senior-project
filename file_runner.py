05b6373d9239d80262513adc8bb6c05be6f9f3f8
PPjaisri <ppjaisri@gmail.com>
2021-12-10 21:13:48 +0700

    update exe file

--- file_runner.py
+++ file_runner.py
@@ -5,15 +5,15 @@ path = os.getcwd()
 path = os.path.dirname(path)
 path = os.path.join(path, 'Scrapy\Scrapy_project')
 
-# os.chdir(path)
+os.chdir(path)
 
-# scrape_list = [
-#     'anti_thread',
-#     'anti_info',
-#     'cofact_thread',
-#     'cofact_info',
-#     'cofact_refer'
-#     ]
+scrape_list = [
+    'anti_thread',
+    'anti_info',
+    'cofact_thread',
+    'cofact_info',
+    'cofact_refer'
+    ]
 
-# for i in scrape_list:
-#     os.system(f'scrapy crawl {i}')
+for i in scrape_list:
+    os.system(f'scrapy crawl {i}')
