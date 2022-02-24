d41410e7553068ba2029d7335a0033263ffb2e7f
PPjaisri <ppjaisri@gmail.com>
2022-02-24 13:23:01 +0700

    remove mongdb file

--- db.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-CONNECTION_STRING = "mongodb+srv://lion:tigerlion007@cluster0.uojms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('Helloworld')
-user_collection = pymongo.collection.Collection(db, 'TestUserInput')
