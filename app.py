a6e56c667899363d42c342103c807712021770d2
Dulyawat Wongurai <blaxlist2017@gmail.com>
2022-03-05 20:15:46 +0700

    use old backend for empty message method

--- app.py
+++ app.py
@@ -68,14 +68,11 @@ class UserExtension(Resource):
             }
             
         elif args["message_type"] == "image":
-            text_from_image = "default"
             try:
                 image_file = args['image']
                 image_file.save("EasyOCR/OCR_User_Pic/tmp.jpg")
                 
                 text_from_image = OCR_with_user_image("EasyOCR/OCR_User_Pic/tmp.jpg")
-                if (len(text_from_image) == 0):
-                    abort(422 ,message = "ไม่พบข้อความในรูปภาพที่ค้นหา")
                 all_result_with_url = cosine_similarity_T(10, text_from_image)
                 
                 queryObject = {
@@ -84,21 +81,15 @@ class UserExtension(Resource):
                     "result": all_result_with_url
                 }
             except:
-                if (len(text_from_image) == 0):
-                    abort(422 ,message = "ไม่พบข้อความในรูปภาพที่ค้นหา")
-                else:
-                    abort(422 ,message = "กรุณาอัพโหลดไฟล์รูปภาพ .JPG หรือ .PNG")
+                abort(422 ,message = "กรุณาอัพโหลดไฟล์รูปภาพ .JPG หรือ .PNG")
             
         elif args["message_type"] == "image_url":
-            text_from_image = "default"
             try:
                 response = requests.get(args["message"])
                 with open('EasyOCR/OCR_User_Pic/tmp.jpg', 'wb') as file:
                     file.write(response.content)
                 
                 text_from_image = OCR_with_user_image("EasyOCR/OCR_User_Pic/tmp.jpg")
-                if (len(text_from_image) == 0):
-                    abort(422 ,message = "ไม่พบข้อความในรูปภาพที่ค้นหา")
                 all_result_with_url = cosine_similarity_T(10, text_from_image)
                 
                 queryObject = {
@@ -107,16 +98,12 @@ class UserExtension(Resource):
                     "result": all_result_with_url
                 }
             except:
-                if (len(text_from_image) == 0):
-                    abort(422 ,message = "ไม่พบข้อความในรูปภาพที่ค้นหา")
-                else:
-                    abort(422 ,message = "ขออภัย เราไม่รองรับลิงค์รูปภาพนี้ กรุณาตรวจสอบลิงค์ หรือบันทึกรูปภาพแล้วค้นหาผ่านการอัพโหลดแทน")
+                abort(422 ,message = "ขออภัย เราไม่รองรับลิงค์รูปภาพนี้ กรุณาตรวจสอบลิงค์ หรือบันทึกรูปภาพแล้วค้นหาผ่านการอัพโหลดแทน")
             
         elif args["message_type"] == "link":
             try:
                 facebook_fetch = facebook(args["message"])
                 post_facebook = facebook_fetch.fetch_page()
-                print("post_facebook", post_facebook)
                 all_result_with_url = cosine_similarity_T(10, post_facebook["content"])
 
                 queryObject = {
